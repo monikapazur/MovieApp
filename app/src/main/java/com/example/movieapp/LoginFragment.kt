68253279eee087fd5c2b51a9b50820f85227c6c0
Monika Pazur <monika.pazur@outlook.com>
2021-12-29 23:24:06 +0100

    log and registration firebase 2

--- app/src/main/java/com/example/movieapp/LoginFragment.kt
+++ app/src/main/java/com/example/movieapp/LoginFragment.kt
@@ -1,13 +1,22 @@
 package com.example.movieapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavHostController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_sign_in.*
 
 
-class LoginFragment : Fragment() {
+class LoginFragment : BaseFragment() {
+
+    private val fbAuth = FirebaseAuth.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,4 +25,43 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupLoginClick()
+        setupRegistrationClick()
+    }
+
+    private fun setupRegistrationClick() {
+        signUpButton.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegistrationFragment().actionId)
+        }
+    }
+
+    private fun setupLoginClick() {
+
+
+        signInButton.setOnClickListener {
+            val email = emailSignIn.text?.trim().toString()
+            val password = passwordSignIn.text?.trim().toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(requireContext(), "fill all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                fbAuth.signInWithEmailAndPassword(email, password)
+                    .addOnSuccessListener { auth ->
+                       if(auth.user != null) startApp()
+
+                    }
+                    .addOnFailureListener { exc ->
+                        Toast.makeText(requireContext(), "sth wrong with log", Toast.LENGTH_SHORT)
+                            .show()
+                        Log.d("log error", exc.message.toString())
+                    }
+            }
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/example/movieapp/RegistrationFragment.kt
+++ app/src/main/java/com/example/movieapp/RegistrationFragment.kt
@@ -1,13 +1,17 @@
 package com.example.movieapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_sign_up.*
 
-class RegistrationFragment : Fragment() {
-
+class RegistrationFragment : BaseFragment() {
+    private val fbAuth = FirebaseAuth.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +21,36 @@ class RegistrationFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        setupSignClick()
+
+    }
+
+    private fun setupSignClick() {
+        signUpButton.setOnClickListener {
+            val email = emailSignUp.text.trim().toString()
+            val password = passwordSignUp.text.trim().toString()
+            val passwordAgain = passwordAgainSignUp.text.trim().toString()
+            if(email.isEmpty() || password.isEmpty() || passwordAgain.isEmpty()){
+                Toast.makeText(requireContext(),"fill all of the fields",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                if(password == passwordAgain){
+                    fbAuth.createUserWithEmailAndPassword(email,password)
+                        .addOnSuccessListener { auth->
+                            if(auth.user != null) startApp()
+
+                        }
+                        .addOnFailureListener { exc->
+                            Toast.makeText(requireContext(), "sth wrong with log", Toast.LENGTH_SHORT)
+                                .show()
+                            Log.d("log error", exc.message.toString())
+                        }
+                }
+
+            }
+        }
+    }
 }
